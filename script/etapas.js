d097e3e40444ee04a8489057c75efb367079b327
Robert Madureira <robeet73@gmail.com>
2021-11-23 21:06:57 -0300

    fist commit

---
+++ script/etapas.js
@@ -0,0 +1,48 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: 88123,
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: './image/88123.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: 89123,
+                name: 'Fulano de Tal',
+                partido: 'DFG',
+                fotos: [
+                    { url: './image/89123.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [{
+                numero: 88,
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: './image/88.jpg', legenda: 'Prefeito' },
+                    { url: './image/88.1', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: 88,
+                name: 'Zulano',
+                partido: 'DFG',
+                vice: 'Zul',
+                fotos: [
+                    { url: './image/89.jpg', legenda: 'Prefeito' },
+                    { url: './image/89.1', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+
+]

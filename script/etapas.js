eee1e6b5e35028778fefc587fe698e5731f1006b
Robert Madureira <robeet73@gmail.com>
2021-11-24 01:39:53 -0300

    fist commit

--- script/etapas.js
+++ script/etapas.js
@@ -2,44 +2,44 @@ let etapas = [{
         titulo: 'VEREADOR',
         numeros: 5,
         candidatos: [{
-                numero: 88123,
-                name: 'Fulano de Tal',
+                numero: '88123',
+                nome: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos: [
-                    { url: './image/88123.jpg', legenda: 'Vereador' }
+                    { url: '88123.jpg', legenda: 'Vereador' }
                 ]
             },
             {
-                numero: 89123,
-                name: 'Fulano de Tal',
+                numero: '89123',
+                nome: 'Beltrano de Tal',
                 partido: 'DFG',
                 fotos: [
-                    { url: './image/89123.jpg', legenda: 'Vereador' }
+                    { url: '89123.jpg', legenda: 'Vereador' }
                 ]
             },
         ]
     },
     {
         titulo: 'PREFEITO',
-        numero: 2,
+        numeros: 2,
         candidatos: [{
-                numero: 88,
-                name: 'Ciclano',
+                numero: '88',
+                nome: 'Ciclano',
                 partido: 'ABC',
                 vice: 'Cic',
                 fotos: [
-                    { url: './image/88.jpg', legenda: 'Prefeito' },
-                    { url: './image/88.1', legenda: 'Vice-Prefeito', small: true }
+                    { url: '88.jpg', legenda: 'Prefeito' },
+                    { url: '88-1.jpg', legenda: 'Vice-Prefeito', small: true }
                 ]
             },
             {
-                numero: 88,
-                name: 'Zulano',
+                numero: '88',
+                nome: 'Zulano',
                 partido: 'DFG',
                 vice: 'Zul',
                 fotos: [
-                    { url: './image/89.jpg', legenda: 'Prefeito' },
-                    { url: './image/89.1', legenda: 'Vice-Prefeito', small: true }
+                    { url: '89.jpg', legenda: 'Prefeito' },
+                    { url: '89-1.jpg', legenda: 'Vice-Prefeito', small: true }
                 ]
             },
         ]
